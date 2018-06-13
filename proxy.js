6163e131df0c6216959a96b320cc847ab45bc92f
Yeoman <mingmingcn.yang@gmail.com>
2018-06-14 02:43:47 +0800

    :sparkles: Support printer ip config

--- proxy.js
+++ proxy.js
@@ -2,30 +2,37 @@ const fs = require('fs');
 const httpProxy = require('http-proxy');
 const ipcMain = require('electron').ipcMain;
 
-ipcMain.on('proxy', function(event, { ipAddress, port }) {
+let lodopProxy;
+let websocketProxy;
+let videoProxy;
+
+ipcMain.on('proxy', function(event, { printerAddress, videoAddress }) {
+    lodopProxy && lodopProxy.close();
     // 代理 lodop
-    httpProxy.createServer({
+    lodopProxy = httpProxy.createServer({
         ssl: {
             key: fs.readFileSync('./cert/titan.key', 'utf8'),
             cert: fs.readFileSync('./cert/titan.crt', 'utf8')
         },
-        target: 'http://localhost:8000',
+        target: `http://${printerAddress}`,
         secure: false
     }).listen(443);
 
+    websocketProxy && websocketProxy.close();
     // 代理 WebSocket
-    httpProxy.createServer({
-        target: 'ws://localhost:8000/c_webskt/',
+    websocketProxy = httpProxy.createServer({
+        target: `ws://${printerAddress}/c_webskt/`,
         ws: true
     }).listen(80);
 
+    videoProxy && videoProxy.close();
     // 代理 视频服务
-    httpProxy.createServer({
+    videoProxy = httpProxy.createServer({
         ssl: {
             key: fs.readFileSync('./cert/titan.key', 'utf8'),
             cert: fs.readFileSync('./cert/titan.crt', 'utf8')
         },
-        target: `http://${ipAddress}:${port}`,
+        target: `http://${videoAddress}`,
         secure: false
     }).listen(8443);
 
--- renderer.js
+++ renderer.js
@@ -3,62 +3,81 @@ const ipcRenderer = require('electron').ipcRenderer;
 ipcRenderer.on('proxy-success', () => {
     alert('重启代理成功');
 });
-
-const testBtn = document.querySelector("#test_btn");
+const testPrinterBtn = document.querySelector("#test_printer_btn");
+const testVideoBtn = document.querySelector("#test_video_btn");
 const saveBtn = document.querySelector("#save_btn");
 const proxyBtn = document.querySelector("#proxy_btn");
+const videoInput = document.querySelector("#video_input");
+const printerInput = document.querySelector("#printer_input");
 
-const ipInput = document.querySelector("#ip_input");
-const portInput = document.querySelector("#port_input");
-
-testBtn.onclick = testVideo;
+testPrinterBtn.onclick = testPrinter;
+testVideoBtn.onclick = testVideo;
 saveBtn.onclick = saveConfig;
 proxyBtn.onclick = restartProxy;
 
-function testVideo() {
-    const ipAddress = ipInput.value;
-    const port = portInput.value;
+function testPrinter() {
+    const ipAddress = printerInput.value;
 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://${ipAddress}:${port}/health`);
+    xhr.open('GET', `http://${ipAddress}/CLodopfuncs.js`);
     xhr.send();
 
-    xhr.onreadystatechange = function () {　　　　
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
-            if (xhr.status == 200) {　　　　　　
+            if (xhr.status == 200) {
+                alert('打印服务器测试通过');
+            } else {
+                alert('请检查打印服务器连接');
+            }
+        }
+    };
+}
+
+function testVideo() {
+    const ipAddress = videoInput.value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://${ipAddress}/health`);
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
                 alert('视频服务器测试通过');
             } else {
-                alert('请检查视频服务器连接');　　　　
+                alert('请检查视频服务器连接');
             }
         }
     };
 }
 
 function saveConfig() {
-    localStorage.setItem('ipAddress', ipInput.value);
-    localStorage.setItem('port', portInput.value);
+    localStorage.setItem('printerAddress', printerInput.value);
+    localStorage.setItem('videoAddress', videoInput.value);
+    alert('保存成功');
 }
 
 function restartProxy() {
-    const ipAddress = ipInput.value;
-    const port = portInput.value;
+    const printerAddress = printerInput.value;
+    const videoAddress = videoInput.value;
 
-    if (!ipAddress || !port) {
+    if (!printerAddress || !videoAddress) {
         return alert('请输入配置');
     }
 
     ipcRenderer.send('proxy', {
-        ipAddress,
-        port
+        printerAddress,
+        videoAddress
     });
 }
 
 (function initConfig() {
-    const ipAddress = localStorage.getItem('ipAddress');
-    const port = localStorage.getItem('port');
+    const printerAddress = localStorage.getItem('printerAddress');
+    const videoAddress = localStorage.getItem('videoAddress');
 
-    ipInput.value = ipAddress;
-    portInput.value = port;
+    printerInput.value = printerAddress;
+    videoInput.value = videoAddress;
 })();
 
 restartProxy();
+
